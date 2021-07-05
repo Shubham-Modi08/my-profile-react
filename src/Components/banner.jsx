fc6dd3d35850f1bd4d7baa54df93b9225e034f09
shubh <mailshubham8995@gmail.com>
2021-07-05 20:37:01 +0530

    banner page done

--- src/Components/banner.jsx
+++ src/Components/banner.jsx
@@ -1,7 +1,7 @@
   
 import React, { Component } from 'react'
 import logo from './images/coding.png';
-// import cse from '../Components/images/yt1.png';
+
 
 
 
@@ -12,8 +12,6 @@ class Banner extends Component {
 
                     <a href="#home"><img id="profilepic" src={logo}  width="170" height="170" alt="profilepic"/></a>
                     <h1> Hi! I'm Shubham Modi </h1>
-                   
-                    {/* <a id="logo" href="https://www.contentstack.com/" ><img src={cse} width="30" height="30" alt="cselogo"/></a>  */}
                     
             </section>
         )
